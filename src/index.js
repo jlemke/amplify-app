68eb422c063de5db020a57f7913e914d38cdf596
jlemke <jlemke3@madisoncollege.edu>
2022-05-07 12:07:30 -0500

    Updated to use newer ui-react
    Changed from legacy to standard

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import reportWebVitals from './reportWebVitals';
 
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
